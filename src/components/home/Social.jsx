d62ab9050d45ba6c92bed32633fe427a9d0d62ab
bishwagitdas <bishwagitdasaiub@gmail.com>
2023-02-27 21:45:14 +0600

    header,home,about,skills component done

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+   <div className="home__social">
+
+
+    <a 
+    href="https://github.com/Bishwagitdas" 
+    className="home__social-icon" 
+    target="blank"
+    >
+    <i class="uil uil-github-alt"></i>
+    </a>
+
+    <a href="https://www.facebook.com/bishwagit.das.3/" 
+    className="home__social-icon" 
+    target="blank"
+    >
+    <i class="uil uil-facebook"></i>
+    </a> 
+
+    
+    <a href="https://www.instagram.com/bishwagit_das/" 
+    className="home__social-icon" 
+    target="blank"
+    >
+    <i class="uil uil-instagram"></i>
+    </a>
+
+    <a href="https://www.linkedin.com/in/bishwagit-das-23563217a/" 
+    className="home__social-icon" 
+    target="blank"
+    >
+    <i class="uil uil-linkedin-alt"></i>
+    </a>
+
+
+   </div>
+  )
+}
+
+export default Social
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./skills.css";
+import Frontend from './Frontend';
+import Backend from './Backend';
+import Database from './Database';
+import Tools from './Tools';
+
+const Skills = () => {
+  return (
+    <section className="skills section" id="skills">
+    <h2 className="section__title">Skills</h2>
+    <span className="section__subtitle">My Technical Level</span>
+
+    <div className="skills__container container grid">
+    <Frontend />
+    <Backend />
+
+    <Database />
+    <Tools />
+    </div>
+    </section>
+
+  )
+}
+
+export default Skills
